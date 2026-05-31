998b182fecc035dc95dfa6fe3c2dfebce8c7ec44
Aura-911 <2425403868@live.gctu.edu.gh>
2026-05-31 13:54:56 +0000

    Add files via upload
    Submitting initials.py assignment.

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+#File: initials.py
+#Description: Print out my initials.
+#Assignment Number: 2
+#
+#Name: Rejoice Amadi Chisom
+#STUDENT ID: 2425403868
+#Email: 2425403868@live.gctu.edu.gh
+#Grader: Mr. Augustus Buckman
+#
+#On my honur, Rejoice Amadi, this programming assignment is my own work
+#and i have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+  # Print out my initials A.R.C in large stylized block letters.
 
+  print()
+  print("...ARC")
+  print()
+
+  print(".......AAAA............RRRRRRRRRR..........CCCCCCCCCCCC.....")
+  print("......AAAAAA...........RRRRRRRRRRRR........CCCCCCCCCCCC.....")
+  print(".....AA....AA..........RR........RR........CCC..............")
+  print("....AA......AA.........RR........RR........CCC..............")
+  print("....AA......AA.........RRRRRRRRRR..........CCC..............")
+  print("...AAAAAAAAAAAA........RRRRRRRRRR..........CCC..............")
+  print("...AA........AA........RR.......RR.........CCC..............")
+  print("...AA........AA........RR........RR........CCC..............")
+  print("...AA........AA...**...RR.........RR..**...CCCCCCCCCCCC...**")
+  print("...AA........AA...**...RR..........RR.**...CCCCCCCCCCCC...**")
+
+  print()
 
 
 main()
